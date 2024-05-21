1b31d2551d722258f7c0a58266f6f0ab50f533d8
Calum Muir <calum@calumx.com>
2024-05-21 08:38:05 +0100

    improve Loading styles

--- src/Loading.js
+++ src/Loading.js
@@ -1,18 +1,25 @@
-import { Box, CircularProgress } from '@mui/material';
+import { Box, CircularProgress, Typography } from '@mui/material';
 
 export const Loading = () => {
   return (
     <Box
       sx={{
-        height: { xs: `calc(env(safe-area-inset-top) + env(safe-area-inset-bottom) + 100vh)` },
-        width: '100vw',
+        zIndex: 2,
+        position: 'absolute',
+        inset: 0,
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: '#71d0f5',
+        color: '#f4c518',
+        flexDirection: 'column',
+        gap: '2rem',
       }}
     >
-      <CircularProgress variant='indeterminate' />
+      <CircularProgress variant='indeterminate' color='inherit' size={100} sx={{ height: '50vh' }} />
+      <Typography variant='h1' color='#fff' fontSize={['2rem', '2.75rem', '3.5rem']} fontWeight={700}>
+        Simpsonsle is loading...
+      </Typography>
     </Box>
   );
 };
